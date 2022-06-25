eb7b14346ea397c32fd81d433bbcec13a1a46aaa
Josué <josue.feldeckircher@cwi.com.br>
2022-06-25 18:51:26 -0300

    implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      fraunces: ["Fraunces", "serif"],
+    },
+    extend: {
+      colors: {
+        "primary-cyan": "hsl(158, 36%, 37%)",
+        "primary-dark-cyan": "hsl(157, 44%, 17%)",
+        
+        "primary-cream": "hsl(30, 38%, 92%)",
+        "dark-blue": "hsl(212, 21%, 14%)",
+        "dark-grayish-blue": "hsl(228, 12%, 48%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 }
